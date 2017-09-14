373f746920745178ba11704485440fa34b6b9547
weijiaqi <84362113@qq.com>
2017-09-15 00:05:39 +0800

    你是最棒的

--- src/main/java/com/coldraincn/laimihui/fragment/ExternalFragment.java
+++ src/main/java/com/coldraincn/laimihui/fragment/ExternalFragment.java
@@ -18,5 +18,6 @@ public class ExternalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.pay_external, container, false);
+        //你是最棒的
     }
 }
